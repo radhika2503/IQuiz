0f88fce43defd7c81e27b1d28b02e0b3c4b10407
radhika2503 <114337855+radhika2503@users.noreply.github.com>
2023-01-12 20:44:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -129,6 +129,8 @@ function optionSelected(answer){
     }else{
         answer.classList.add("incorrect"); //adding red color to correct selected option
         answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+        userScore -= 1;
+
         console.log("Wrong Answer");
 
         for(i=0; i < allOptions; i++){
@@ -150,17 +152,25 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 3
+    if (userScore > 159){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p></span>';
+        let scoreTag = '<span>and congrats!you are genius, You got <p>'+ userScore +'</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
     }
-    else if(userScore > 1){ // if user scored more than 1
-        let scoreTag = '<span>and nice 😎, You got <p>'+ userScore +'</p> </span>';
+    else if(userScore > 145 ){ // if user scored more than 1
+        let scoreTag = '<span>HIGH Score, You got <p>'+ userScore +'</p> </span>';
         scoreText.innerHTML = scoreTag;
     }
+    else if(userScore > 116){ // if user scored more than 1
+        let scoreTag = '<span>Above Average , You got <p>'+ userScore +'</p> </span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 71){ // if user scored more than 1
+        let scoreTag = '<span>Average , You got <p>'+ userScore +'</p> </span>';
+        scoreText.innerHTML = scoreTag;
+    }    
     else{ // if user scored less than 1
-        let scoreTag = '<span>and sorry 😐, You got only <p>'+ userScore +'</span>';
+        let scoreTag = '<span>You need to work a little bit, You got only <p>'+ userScore +'</span>';
         scoreText.innerHTML = scoreTag;
     }
 }
